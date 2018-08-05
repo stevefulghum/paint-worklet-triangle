c9d820a591f945c26d4e1f953a17b02896415766
Steve Fulghum <steve@fulghum.digtal>
2018-08-05 15:29:57 -0400

    migrate from codepen

--- triangleWorklet.js
+++ triangleWorklet.js
@@ -1,20 +1,60 @@
-class trianglePainter {
-    paint(ctx, geom, properties) {
-      let midpoint = geom.width / 2;
-      let bottom = geom.height;
-      let right = geom.width;
-      
-      ctx.beginPath();
-      ctx.moveTo(0, bottom);
-      ctx.lineTo(midpoint, 0);
-      ctx.lineTo(right, bottom);
-      ctx.lineTo(0, bottom);
-      ctx.closePath();
-      
-      ctx.fillStyle = 'green';
-      ctx.fill();
-    }
+class TrianglePainter {
+
+  static get inputProperties() {
+    return ['--arrow-pos-left', '--arrow-height', '--arrow-width', '--arrow-fill', '--arrow-stroke'];
   }
   
-  registerPaint('triangle', trianglePainter);
+  paint(ctx, geom, properties, args) {
+    const widthProp = properties.get('--arrow-width');
+    const heightProp = properties.get('--arrow-height');
+    const fill = properties.get('--arrow-fill');
+    const stroke = properties.get('--arrow-stroke');
+    
+    let width;
+    let height;
+    let midpoint;
+    
+    if (!widthProp.unit) {
+      console.log('is not fancy');
+      
+      width = parseInt(widthProp[0], 10);
+      height = parseInt(heightProp[0], 10);
+      
+      
+      // let derp = width;
+    } else {
+      width = widthProp.value;
+      height = heightProp.value;
+    }
+    
+    midpoint = width / 2;
+    
+    console.log(width);
+    console.log(height);
+    console.log(midpoint);
+    
+    ctx.beginPath();
+    ctx.moveTo(1, (height + 1));
+    ctx.lineTo(midpoint, 3);
+    ctx.lineTo((width - 1), (height + 1));
+    ctx.lineTo(1, (height + 1));
+    ctx.closePath();
+    
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = stroke.toString();
+    ctx.stroke();
+    
+    ctx.fillStyle = fill.toString();
+    ctx.fill();
+  }
   
+  parseCSSPxLength(value) {
+    let slicer = -2;
+    let parsed = value.slice(slicer);
+    
+    return parsed;
+  }
+}
+
+registerPaint('triangle', TrianglePainter);
+
